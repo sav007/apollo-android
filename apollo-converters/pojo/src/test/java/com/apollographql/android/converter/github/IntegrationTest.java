23d13fc6b622f04c1b8821f4e79e9f35282cee3a
Ivan Savytskyi <ivan.savitsky@gmail.com>
2017-02-04 19:23:52 -0500

    Refactor

--- apollo-converters/pojo/src/test/java/com/apollographql/android/converter/github/IntegrationTest.java
+++ apollo-converters/pojo/src/test/java/com/apollographql/android/converter/github/IntegrationTest.java
@@ -1,11 +1,8 @@
 package com.apollographql.android.converter.github;
 
 import com.google.common.base.Charsets;
-import com.google.common.base.Function;
-import com.google.common.collect.FluentIterable;
 import com.google.common.io.Files;
 
-import com.apollographql.android.api.graphql.Error;
 import com.apollographql.android.api.graphql.Response;
 import com.apollographql.android.converter.github.type.CustomType;
 import com.apollographql.android.converter.pojo.ApolloConverterFactory;
@@ -21,10 +18,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 
 import okhttp3.mockwebserver.MockResponse;
@@ -103,58 +97,6 @@ public class IntegrationTest {
     Call<Response<UserWithOrganizationsQuery.Data>> call = service.heroDetails(new OperationRequest<>(query));
     Response<UserWithOrganizationsQuery.Data> body = call.execute().body();
     assertThat(body.isSuccessful()).isTrue();
-
-//    assertThat(server.takeRequest().getBody().readString(Charsets.UTF_8))
-//        .isEqualTo("{\"query\":\"query TestQuery {  "
-//            + "allPlanets(first: 300) {"
-//            + "    planets {"
-//            + "      ...PlanetFragment"
-//            + "      filmConnection {"
-//            + "        totalCount"
-//            + "        films {"
-//            + "          title"
-//            + "          ...FilmFragment"
-//            + "        }"
-//            + "      }"
-//            + "    }"
-//            + "  }"
-//            + "}"
-//            + "fragment PlanetFragment on Planet {"
-//            + "  name"
-//            + "  climates"
-//            + "  surfaceWater"
-//            + "}"
-//            + "fragment FilmFragment on Film {"
-//            + "  title"
-//            + "  producers"
-//            + "}\",\"variables\":{}}");
-//
-//    AllPlanets.Data data = body.data();
-//    assertThat(data.allPlanets().planets().size()).isEqualTo(60);
-//
-//    List<String> planets = FluentIterable.from(data.allPlanets().planets())
-//        .transform(new Function<AllPlanets.Data.AllPlanet.Planet, String>() {
-//          @Override public String apply(AllPlanets.Data.AllPlanet.Planet planet) {
-//            return planet.fragments().planetFargment().name();
-//          }
-//        }).toList();
-//    assertThat(planets).isEqualTo(Arrays.asList(("Tatooine, Alderaan, Yavin IV, Hoth, Dagobah, Bespin, Endor, Naboo, "
-//        + "Coruscant, Kamino, Geonosis, Utapau, Mustafar, Kashyyyk, Polis Massa, Mygeeto, Felucia, Cato Neimoidia, "
-//        + "Saleucami, Stewjon, Eriadu, Corellia, Rodia, Nal Hutta, Dantooine, Bestine IV, Ord Mantell, unknown, "
-//        + "Trandosha, Socorro, Mon Cala, Chandrila, Sullust, Toydaria, Malastare, Dathomir, Ryloth, Aleen Minor, "
-//        + "Vulpter, Troiken, Tund, Haruun Kal, Cerea, Glee Anselm, Iridonia, Tholoth, Iktotch, Quermia, Dorin, "
-//        + "Champala, Mirial, Serenno, Concord Dawn, Zolan, Ojom, Skako, Muunilinst, Shili, Kalee, Umbara")
-//        .split("\\s*,\\s*")
-//    ));
-//
-//    AllPlanets.Data.AllPlanet.Planet firstPlanet = data.allPlanets().planets().get(0);
-//    assertThat(firstPlanet.fragments().planetFargment().climates()).isEqualTo(Collections.singletonList("arid"));
-//    assertThat(firstPlanet.fragments().planetFargment().surfaceWater()).isWithin(1d);
-//    assertThat(firstPlanet.filmConnection().totalCount()).isEqualTo(5);
-//    assertThat(firstPlanet.filmConnection().films().size()).isEqualTo(5);
-//    assertThat(firstPlanet.filmConnection().films().get(0).fragments().filmFragment().title()).isEqualTo("A New Hope");
-//    assertThat(firstPlanet.filmConnection().films().get(0).fragments().filmFragment().producers()).isEqualTo(Arrays
-//        .asList("Gary Kurtz", "Rick McCallum"));
   }
 
   private static MockResponse mockResponse(String fileName) throws IOException {
